9c4da19e610219215cf604d07405215707016636
JefersonCastro17 <jefersonjairbernalcastro172129@gmail.com>
2026-08-23 15:49:54 -0500

    chore: initial commit with monorepo setup and gitignore

---
+++ Propio/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    ...(process.env.CYPRESS_COVERAGE === 'true'
+      ? [istanbul({ cypress: true, requireEnv: false })]
+      : []),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
